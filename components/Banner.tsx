a880a3332905cf875373c6714ab82b9d9edb9b28
Juan P. Romano <programming@jpromano.net>
2024-06-22 19:06:51 -0300

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Banner(): JSX.Element {
   return (
-    <div className="flex justify-center space-x-1 bg-zinc-100 p-2 text-xs dark:bg-zinc-900 md:text-sm lg:text-base">
+    <div>
      
     </div>
   );
